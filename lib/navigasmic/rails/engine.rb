25dd696a4ea11f07de8deee079faf935eb79762c
jejacks0n <jejacks0n@gmail.com>
2012-10-02 21:22:09 -0600

    fixes a bug in the engine where it was requiring a file that might not be there.

--- lib/navigasmic/rails/engine.rb
+++ lib/navigasmic/rails/engine.rb
@@ -5,5 +5,11 @@ module Navigasmic
     initializer "navigasmic.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
     end
+
+    config.to_prepare do
+      if File.exists?(Rails.root.join('config', 'initializers', 'navigasmic'))
+        require Rails.root.join('config', 'initializers', 'navigasmic')
+      end
+    end
   end
 end
