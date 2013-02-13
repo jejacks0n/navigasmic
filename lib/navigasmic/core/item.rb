3d5f02505ad0237e9fc7dfa10a24b71ac9c1daa2
Corin Langosch <info@corinlangosch.com>
2013-02-13 18:11:00 +0100

    optimized #highlights_on?

--- lib/navigasmic/core/item.rb
+++ lib/navigasmic/core/item.rb
@@ -22,29 +22,28 @@ class Navigasmic::Item
   end
 
   def highlights_on?(path, params)
+    return false unless @rules.any?
     params = clean_unwanted_keys(params)
-    result = false
-
     @rules.each do |rule|
-      highlighted = true
-
       case rule
-        when String then highlighted &= path == rule
-        when Regexp then highlighted &= path.match(rule)
-        when TrueClass then highlighted &= rule
-        when FalseClass then highlighted &= rule
-        when Hash
-          clean_unwanted_keys(rule).each do |key, value|
-            value.gsub(/^\//, '') if key == :controller
-            highlighted &= value == params[key].to_s
-          end
-        else raise 'highlighting rules should be an array containing any of/or a Boolean, String, Regexp, Hash or Proc'
+      when String
+        return false unless path == rule
+      when Regexp
+        return false unless path.match(rule)
+      when TrueClass
+        # no-op
+      when FalseClass
+        return false
+      when Hash
+        clean_unwanted_keys(rule).each do |key, value|
+          value.gsub(/^\//, '') if key == :controller
+          return false unless value == params[key].to_s
+        end
+      else
+        raise ArgumentError, 'Highlighting rules should be an array containing any of/or a Boolean, String, Regexp, Hash or Proc'
       end
-
-      result |= highlighted
     end
-
-    result
+    true
   end
 
   private
