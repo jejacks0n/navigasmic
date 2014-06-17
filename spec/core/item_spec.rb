0f903dc87b955498f0c9cbe13bde0316e5a21c8c
Daniel Hanson <hansondr@gmail.com>
2014-06-17 17:10:18 -0600

    refactored spec expectations using `should` over to `expect().to`

--- spec/core/item_spec.rb
+++ spec/core/item_spec.rb
@@ -7,105 +7,105 @@ describe Navigasmic::Item do
   describe "#hidden?" do
     it "returns true when it's not visible" do
       item = subject.new 'Label', {controller: 'controller'}, false
-      item.hidden?.should be(true)
+      expect(item.hidden?).to be true
     end
 
     it "returns false when it's visible" do
       item = subject.new 'Label', {controller: 'controller'}, true
-      item.hidden?.should be(false)
+      expect(item.hidden?).to be false
     end
   end
 
   describe "#disabled?" do
     it "returns true when it's disabled" do
       item = subject.new 'Label', {controller: 'controller'}, true, disabled_if: true
-      item.disabled?.should be(true)
+      expect(item.disabled?).to be true
     end
 
     it "returns false when it's not disabled" do
       item = subject.new 'Label', {controller: 'controller'}, true, disabled_if: false
-      item.disabled?.should be(false)
+      expect(item.disabled?).to be false
     end
   end
 
   describe "#link?" do
     it "returns true when there's a link" do
       item = subject.new 'Label', 'url', true
-      item.link?.should be(true)
+      expect(item.link?).to be true
     end
 
     it "returns false when there isn't a link" do
       item = subject.new 'Label', nil, true
-      item.link?.should be(false)
+      expect(item.link?).to be false
     end
 
     it "returns false when it's disabled" do
       item = subject.new 'Label', nil, true, disabled_if: true
-      item.link?.should be(false)
+      expect(item.link?).to be false
     end
   end
 
   describe "#calculate_highlighting_rules" do
     it "uses the item's link when no rules given" do
       item = subject.new 'Label', 'url', true
-      item.send(:calculate_highlighting_rules, nil).should eq(['url'])
+      expect(item.send(:calculate_highlighting_rules, nil)).to eq(['url'])
     end
 
     it "uses the given rules if any given" do
       item = subject.new 'Label', 'url', true, :highlights_on => false
-      item.send(:calculate_highlighting_rules, false).should eq([false])
+      expect(item.send(:calculate_highlighting_rules, false)).to eq([false])
 
       item = subject.new 'Label', 'url', true, :highlights_on => false
-      item.send(:calculate_highlighting_rules, ["/a", "/b"]).should eq(["/a", "/b"])
+      expect(item.send(:calculate_highlighting_rules, ["/a", "/b"])).to eq(["/a", "/b"])
     end
   end
 
   describe "#highlights_on?" do
     it "uses it's own link (as a string)" do
       item = subject.new 'Label', '/path', true
-      item.highlights_on?('/path', {}).should be(true)
-      item.highlights_on?('/other_path', {}).should be(false)
+      expect(item.highlights_on?('/path', {})).to be true
+      expect(item.highlights_on?('/other_path', {})).to be false
     end
 
     it "uses it's own path (as hash)" do
       item = subject.new 'Label', {controller: 'foo'}, true
-      item.highlights_on?('/path', {controller: 'foo'}).should be(true)
-      item.highlights_on?('/other_path', {controller: 'bar'}).should be(false)
+      expect(item.highlights_on?('/path', {controller: 'foo'})).to be true
+      expect(item.highlights_on?('/other_path', {controller: 'bar'})).to be false
     end
 
     it "highlights on multiple controllers" do
       item = subject.new 'Label', '/foo', true, highlights_on: [{controller: 'foo'}, {controller: 'bar'}]
-      item.highlights_on?('/path', {controller: 'foo'}).should be(true)
-      item.highlights_on?('/other_path', {controller: 'bar'}).should be(true)
-      item.highlights_on?('/other_path_entirely', {controller: 'baz'}).should be(false)
+      expect(item.highlights_on?('/path', {controller: 'foo'})).to be true
+      expect(item.highlights_on?('/other_path', {controller: 'bar'})).to be true
+      expect(item.highlights_on?('/other_path_entirely', {controller: 'baz'})).to be false
     end
 
     it "handles strings" do
       item = subject.new 'Label', '/foo', true, highlights_on: '/path'
-      item.highlights_on?('/path', {}).should be(true)
-      item.highlights_on?('/other_path', {}).should be(false)
+      expect(item.highlights_on?('/path', {})).to be true
+      expect(item.highlights_on?('/other_path', {})).to be false
     end
 
     it "handles regexp" do
       item = subject.new 'Label', '/foo', true, highlights_on: /^\/pa/
-      item.highlights_on?('/path', {}).should be(true)
-      item.highlights_on?('/other_path', {}).should be(false)
+      expect(item.highlights_on?('/path', {})).to be true
+      expect(item.highlights_on?('/other_path', {})).to be false
     end
 
     it "handles true/false" do
       item = subject.new 'Label', '/foo', true, highlights_on: true
-      item.highlights_on?('/path', {}).should be(true)
-      item.highlights_on?('/other_path', {}).should be(true)
+      expect(item.highlights_on?('/path', {})).to be true
+      expect(item.highlights_on?('/other_path', {})).to be true
 
       item = subject.new 'Label', '/foo', true, highlights_on: false
-      item.highlights_on?('/path', {}).should be(false)
-      item.highlights_on?('/other_path', {}).should be(false)
+      expect(item.highlights_on?('/path', {})).to be false
+      expect(item.highlights_on?('/other_path', {})).to be false
     end
 
     it "leaves controller paths alone (bug fix)" do
       item = subject.new 'Label', {controller: '/foo'}, true
       item.highlights_on?('/path', {})
-      item.link.should == {controller: '/foo'}
+      expect(item.link).to eq({controller: '/foo'})
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV['RAILS_ROOT'] = File.expand_path('../dummy', __FILE__)
 require File.expand_path('../dummy/config/environment', __FILE__)
 
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
